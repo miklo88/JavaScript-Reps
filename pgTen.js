d95b620549e98c3bae11a12d1b68b2e6b2baf75b
Carlos <miklo.concepcion@gmail.com>
2020-04-19 21:14:29 -0500

    fluffed up html and css page to accomodate another form. I want to have all of the pages rendering in the browser so you can see what is going on also. It makes you change up the syntax and create new values so you have to utilize this knowlegde in more creative ways which reinforces the leanring. Page eleven implemented and finished. This is basically working with functions and eventListeners and manipulating elements on the DOM. Powerful fun stuff. My styling needs this badly.

--- pgTen.js
+++ pgTen.js
@@ -1,11 +1,11 @@
 const titleTen = () => "///////////// THIS IS PAGE TEN /////////////";
 console.log(titleTen());
-///// DOM manipulation
+// ///// DOM manipulation
 
-///////////////// selecting elements from the DOM
+// ///////////////// selecting elements from the DOM
 // Single Element
 // grabbing the form
-console.log(document.getElementById("my-form"));
+console.log(document.getElementById("form-ten"));
 console.log(document.querySelector("h1"));
 
 // Multiple Element
@@ -13,12 +13,12 @@ console.log(document.querySelector("h1"));
 console.log(document.querySelectorAll("h1"));
 
 /////////// looping through elements
-const items = document.querySelectorAll(".item");
-items.forEach((item) => console.log(item));
+const things = document.querySelectorAll(".thing");
+things.forEach((thing) => console.log(thing));
 
 //////// manipulation of dom elements using javascript methods.
 /// a lot of this is why React was created but is still very important to know! trust me, my struggle bus has been vertical
-const ul = document.querySelector(".items");
+const ul = document.querySelector(".things");
 // ul.remove();
 // ul.lastElementChild.remove();
 //changes the first li to hello
@@ -28,15 +28,15 @@ ul.children[1].innerText = "Sneaky";
 // changes the last element in the ul list  to an h1 with the text.
 ul.lastElementChild.innerHTML = "<h1>Wakiki Sneaky</h1>";
 // working with styles
-const btn = document.querySelector(".btn");
-btn.style.background = "purple";
+const button = document.querySelector(".button");
+button.style.background = "red";
 
 // event listener
 // always use addEventListener which takes two things.
 /// first is the event, second is going to be the function that you want to run
 // when the button is clicked.
 // takes an event 'e'.
-// btn.addEventListener("click", (e) => {
+// button.addEventListener("click", (e) => {
 //   //e.preventDefault() prevents from the click to dissapear in the console.
 //     e.preventDefault();
 //   // console.log(e) shows you the event object
@@ -45,9 +45,10 @@ btn.style.background = "purple";
 // // you can gather all the different attributes of the event you click.
 //     console.log(e.target.className);
 // });
-btn.addEventListener("click", (e) => {
+button.addEventListener("click", (e) => {
   e.preventDefault();
-  document.querySelector("#my-form").style.background = "#ccc";
+  document.querySelector("#form-ten").style.background = "#ccc";
   document.querySelector("body").classList.add("bg-dark");
-  document.querySelector(".items").lastElementChild.innerHTML = "<h1>Hola</h1>";
+  document.querySelector(".things").lastElementChild.innerHTML =
+    "<h1>Hola</h1>";
 });
